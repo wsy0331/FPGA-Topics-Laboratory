9b7555920496bc0f0bc5b7188ae9fbe89ad7afc1
wsy0331 <swu33606@gmail.com>
2025-09-18 17:12:17 +0800

    feat: 新增三個計數器輪流計數

--- Ex1_Two_counts/Ex1.vhd
+++ Ex1_Two_counts/Ex1.vhd
@@ -4,22 +4,25 @@ use IEEE.numeric_std.all;
 use IEEE.std_logic_unsigned.all;
 
 entity Ex1 is
-    Port ( i_clk : in STD_LOGIC;
-           i_rst : in STD_LOGIC;
-           o_count1 : out STD_LOGIC_VECTOR (7 downto 0);
-           o_count2 : out STD_LOGIC_VECTOR (7 downto 0));
+    Port ( i_clk       : in STD_LOGIC;
+                   i_rst        : in STD_LOGIC;
+                  o_count1 : out STD_LOGIC_VECTOR (7 downto 0);
+                  o_count2 : out STD_LOGIC_VECTOR (7 downto 0);
+                  o_count3 : out STD_LOGIC_VECTOR (7 downto 0));
 end Ex1;
 
 architecture Behavioral of Ex1 is
     signal count1 : STD_LOGIC_VECTOR (7 downto 0);
     signal count2 : STD_LOGIC_VECTOR (7 downto 0);
-    type FSM_state is (S0, S1);
+    signal count3 : STD_LOGIC_VECTOR (7 downto 0);
+    type FSM_state is (S0, S1, S2);
     signal state : FSM_state;
 begin
     o_count1 <= count1;
     o_count2 <= count2;
+    o_count3 <= count3;
     
-    FSM: process(i_clk, i_rst, count1, count2)
+    FSM: process(i_clk, i_rst, count1, count2, count3)
     begin
         if i_rst = '0' then
             state <= S0;
@@ -31,6 +34,10 @@ begin
                     end if;
                 when S1 =>
                     if count2 = "01001111" then
+                        state <= S2;
+                    end if;
+                when S2 =>
+                    if count3 = "00011110" then
                         state <= S0;
                     end if;
                 when others =>
@@ -46,7 +53,7 @@ begin
         elsif i_clk'event and i_clk = '1' then
             case state is
                 when S0 =>
-                    if count2 = "01001111" then
+                    if count3 = "00011110" then
                         count1 <= count1;
                     elsif count1 < "00001001" then
                         count1 <= count1 + '1';
@@ -54,6 +61,8 @@ begin
                         count1 <= count1;
                     end if;
                 when S1 =>
+                    count1 <= "00000000";
+                when S2 =>
                     count1 <= "00000000";
                 when others =>
                     null;
@@ -77,10 +86,36 @@ begin
                     else
                         count2 <= count2;
                     end if;
+                when S2 =>
+                    count2 <= "11111101";
                 when others =>
                     null;
             end case;
         end if;
     end process counter2;
     
+    counter3: process(i_clk, i_rst, state)
+    begin
+        if i_rst = '0' then
+            count3 <= "00001111";
+        elsif i_clk'event and i_clk = '1' then
+            case state is
+                when S0 =>
+                    count3 <= "00001111";
+                when S1 =>
+                    count3 <= "00001111";
+                when S2 =>
+                    if count2 = "01001111" then
+                        count3 <= count3;
+                    elsif count3 < "00011110" then
+                        count3 <= count3 + '1';
+                    else
+                        count3 <= count3;
+                    end if;
+                when others =>
+                    null;
+            end case;
+        end if;
+    end process counter3;
+       
 end Behavioral;
--- Ex1_Two_counts/Ex1_tb.vhd
+++ Ex1_Two_counts/Ex1_tb.vhd
@@ -8,28 +8,27 @@ end Ex1_tb;
 
 architecture Behavioral of Ex1_tb is
     component Ex1 
-        Port ( 
-            i_clk    : in STD_LOGIC;
-            i_rst    : in STD_LOGIC;
-            o_count1 : out STD_LOGIC_VECTOR (7 downto 0);
-            o_count2 : out STD_LOGIC_VECTOR (7 downto 0)
-        );
+        Port ( i_clk        : in STD_LOGIC;
+                           i_rst         : in STD_LOGIC;
+                           o_count1 : out STD_LOGIC_VECTOR (7 downto 0);
+                           o_count2 : out STD_LOGIC_VECTOR (7 downto 0);
+                           o_count3 : out STD_LOGIC_VECTOR (7 downto 0));
     end component;
-
     signal i_clk    : STD_LOGIC := '0';
     signal i_rst    : STD_LOGIC := '0';
     signal o_count1 : STD_LOGIC_VECTOR (7 downto 0);
     signal o_count2 : STD_LOGIC_VECTOR (7 downto 0);
-
+    signal o_count3 : STD_LOGIC_VECTOR (7 downto 0);
+    
 begin
 
-    -- ±N DUT¡]design under test¡^±µ¤W signal
     dut: Ex1 
         port map (
             i_clk    => i_clk,
             i_rst    => i_rst,
             o_count1 => o_count1,
-            o_count2 => o_count2
+            o_count2 => o_count2,
+            o_count3 => o_count3
         );
 
     clock_process : process
