66317f2030858f6576ec999c804fdbe26c538401
wsy0331 <swu33606@gmail.com>
2025-09-18 16:27:43 +0800

    feat: 新增兩個計數器輪流計數

---
+++ Ex1_Two_counts/Ex1.vhd
@@ -0,0 +1,86 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+use IEEE.std_logic_unsigned.all;
+
+entity Ex1 is
+    Port ( i_clk : in STD_LOGIC;
+           i_rst : in STD_LOGIC;
+           o_count1 : out STD_LOGIC_VECTOR (7 downto 0);
+           o_count2 : out STD_LOGIC_VECTOR (7 downto 0));
+end Ex1;
+
+architecture Behavioral of Ex1 is
+    signal count1 : STD_LOGIC_VECTOR (7 downto 0);
+    signal count2 : STD_LOGIC_VECTOR (7 downto 0);
+    type FSM_state is (S0, S1);
+    signal state : FSM_state;
+begin
+    o_count1 <= count1;
+    o_count2 <= count2;
+    
+    FSM: process(i_clk, i_rst, count1, count2)
+    begin
+        if i_rst = '0' then
+            state <= S0;
+        elsif i_clk'event and i_clk = '1' then
+            case state is
+                when S0 =>
+                    if count1 = "00001001" then
+                        state <= S1;
+                    end if;
+                when S1 =>
+                    if count2 = "01001111" then
+                        state <= S0;
+                    end if;
+                when others =>
+                    null;
+            end case;
+        end if;
+    end process FSM;
+    
+    counter1: process(i_clk, i_rst, state)
+    begin
+        if i_rst = '0' then
+            count1 <= "00000000";
+        elsif i_clk'event and i_clk = '1' then
+            case state is
+                when S0 =>
+                    if count2 = "01001111" then
+                        count1 <= count1;
+                    elsif count1 < "00001001" then
+                        count1 <= count1 + '1';
+                    else 
+                        count1 <= count1;
+                    end if;
+                when S1 =>
+                    count1 <= "00000000";
+                when others =>
+                    null;
+            end case;
+        end if;
+    end process counter1;
+    
+    counter2: process(i_clk, i_rst, state)
+    begin
+        if i_rst = '0' then
+            count2 <= "11111101";
+        elsif i_clk'event and i_clk = '1' then
+            case state is
+                when S0 =>
+                    count2 <= "11111101";
+                when S1 =>
+                    if count1 = "00001001" then
+                        count2 <= count2;
+                    elsif count2 > "01001111" then
+                        count2 <= count2 - '1';
+                    else
+                        count2 <= count2;
+                    end if;
+                when others =>
+                    null;
+            end case;
+        end if;
+    end process counter2;
+    
+end Behavioral;
---
+++ Ex1_Two_counts/Ex1_tb.vhd
@@ -0,0 +1,51 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+use IEEE.std_logic_unsigned.all;
+
+entity Ex1_tb is
+end Ex1_tb;
+
+architecture Behavioral of Ex1_tb is
+    component Ex1 
+        Port ( 
+            i_clk    : in STD_LOGIC;
+            i_rst    : in STD_LOGIC;
+            o_count1 : out STD_LOGIC_VECTOR (7 downto 0);
+            o_count2 : out STD_LOGIC_VECTOR (7 downto 0)
+        );
+    end component;
+
+    signal i_clk    : STD_LOGIC := '0';
+    signal i_rst    : STD_LOGIC := '0';
+    signal o_count1 : STD_LOGIC_VECTOR (7 downto 0);
+    signal o_count2 : STD_LOGIC_VECTOR (7 downto 0);
+
+begin
+
+    -- ±N DUT¡]design under test¡^±µ¤W signal
+    dut: Ex1 
+        port map (
+            i_clk    => i_clk,
+            i_rst    => i_rst,
+            o_count1 => o_count1,
+            o_count2 => o_count2
+        );
+
+    clock_process : process
+    begin
+        i_clk <= '0';
+        wait for 10 ns;
+        i_clk <= '1';
+        wait for 10 ns;
+    end process;
+
+    stim_process: process
+    begin
+        i_rst <= '0';
+        wait for 20 ns;
+        i_rst <= '1'; 
+        wait;
+    end process;
+
+end Behavioral;
